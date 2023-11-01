5d77246e73e9fc43511a5918205cd356de515c08
Michaelsadek <michealsadek849@gmail.com>
2023-10-31 19:46:03 -0700

    a

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def islower(c):
 def uppercase(str):
     for i in str:
-        if ord("a") <= ord(i) <= ord("z"):
+        if ord("a") <= ord(i) <= ord("Z"):
             i = chr(ord(i) - 32)
         print("{:s}".format(i), end="")
-    print()
+    print("")
