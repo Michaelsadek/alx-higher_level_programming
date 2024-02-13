d74e81c48eb6c6ebdd4a7599138eddde6471119e
Michaelsadek <michealsadek849@gmail.com>
2024-02-13 11:30:30 -0800

    a

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
+
 const list = require('./100-data.js').list;
 console.log(list);
 console.log(list.map((item, index) => item * index));
+
+const array1 = [1, 4, 9, 16];
+
+// Pass a function to map
+const map1 = array1.map((x) => x * 2);
+
+console.log(map1);
+// Expected output: Array [2, 8, 18, 32]
